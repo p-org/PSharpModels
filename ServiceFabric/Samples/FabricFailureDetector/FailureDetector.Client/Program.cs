86811c08c3b47de3f14387d77dc891d99c902ea2
Rashmi <t-rasmud@microsoft.com>
2016-06-07 09:25:25 +0530

    fabric failure detector

---
+++ ServiceFabric/Samples/FabricFailureDetector/FailureDetector.Client/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.ServiceFabric.Actors;
+using Microsoft.ServiceFabric.Actors.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using FailureDetector.Interfaces;
+
+namespace FailureDetector.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var senderProxy = ActorProxy.Create<IDriver>(new ActorId(0), "fabric:/FabricFailureDetector");
+            Console.WriteLine("DONE!!!");
+        }
+    }
+}
